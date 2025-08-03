a2d40280bda7b588473788b38f1dc10ce875f3e3
GMRafay <arafay0110@gmail.com>
2025-08-03 01:33:15 -0600

    added styling to the login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,39 @@
-import { Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function LoginPage() {
-  return <Text>You have reached the login screen</Text>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <View className="flex flex-col items-center justify-center h-full w-full">
+      <Text>Login</Text>
+      <LinearGradient
+        colors={["#3F72AF", "#FFFFFF"]} // indigo to white
+        className="rounded-2xl w-[70%] h-[50%] flex flex-col items-center justify-center gap-5 border border-black overflow-hidden"
+      >
+        <Text className="text-center text-xl text-[#F9F7F7]">
+          Sign in with email
+        </Text>
+        <Text className="text-center text-[#F9F7F7]">
+          Make your fitness journey real by tracking your progress.
+        </Text>
+        
+        <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
+          <TextInput
+            className="text-start text-black bg-transparent"
+            placeholder="Email"
+            onChangeText={setEmail}
+          />
+        </View>
+        <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
+        <TextInput
+          placeholder="Password"
+          onChangeText={setPassword}
+        />
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
