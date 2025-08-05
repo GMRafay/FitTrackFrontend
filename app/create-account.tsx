6dd075772ddd57f0f004ce5569166be99b09977f
GMRafay <arafay0110@gmail.com>
2025-08-04 23:00:01 -0600

    added navigation between login and create account pages

--- app/create-account.tsx
+++ app/create-account.tsx
@@ -1,8 +1,42 @@
-import { Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
-export default function CreateAccountPage () {
+export default function CreateAccountPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  return (
+    <View className="flex flex-col items-center justify-center h-full w-full shadow-xl ">
+      <Text>Create account</Text>
+      <LinearGradient
+        colors={["#3F72AF", "#FFFFFF"]} // indigo to white
+        className="rounded-2xl w-[70%] h-[50%] flex flex-col items-center justify-center gap-5 border border-black overflow-hidden"
+      >
+        <Text className="text-center text-xl text-[#F9F7F7]">
+          Sign in with email
+        </Text>
+        <Text className="text-center text-[#F9F7F7]">
+          Make your fitness journey real by tracking your progress.
+        </Text>
 
-    return (
-        <Text>You have reached account creation</Text>
-    );
+        <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
+          <TextInput
+            className="text-start text-black bg-transparent"
+            placeholder="Email"
+            onChangeText={setEmail}
+          />
+        </View>
+        <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
+          <TextInput placeholder="Password" onChangeText={setPassword} />
+        </View>
+        <Text>Dont have an account?</Text>
+        <Button
+          title="Create Account"
+          onPress={() => router.navigate("/create-account")}
+        ></Button>
+      </LinearGradient>
+    </View>
+  );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,15 @@
 import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function LoginPage() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const router = useRouter();
 
   return (
-    <View className="flex flex-col items-center justify-center h-full w-full">
+    <View className="flex flex-col items-center justify-center h-full w-full shadow-xl ">
       <Text>Login</Text>
       <LinearGradient
         colors={["#3F72AF", "#FFFFFF"]} // indigo to white
@@ -19,7 +21,7 @@ export default function LoginPage() {
         <Text className="text-center text-[#F9F7F7]">
           Make your fitness journey real by tracking your progress.
         </Text>
-        
+
         <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
           <TextInput
             className="text-start text-black bg-transparent"
@@ -28,11 +30,13 @@ export default function LoginPage() {
           />
         </View>
         <View className="flex flex-row w-[80%] border border-black rounded-2xl bg-[#DBE2EF]">
-        <TextInput
-          placeholder="Password"
-          onChangeText={setPassword}
-        />
+          <TextInput placeholder="Password" onChangeText={setPassword} />
         </View>
+        <Text>Dont have an account?</Text>
+        <Button
+          title="Create Account"
+          onPress={() => router.navigate("/create-account")}
+        ></Button>
       </LinearGradient>
     </View>
   );
